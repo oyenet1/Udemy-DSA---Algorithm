0a9f046093ef3600a4a18bca4b3bcba1d796f078
oyenet1 <bowofadeoyerinde@gmail.com>
2023-01-15 14:56:24 +0100

    first commit

---
+++ binarySearch.js
@@ -0,0 +1,18 @@
+let binarySearch = (arr, el) => {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        if (arr[middle] < el) {
+            start = middle + 1;
+        } else if (arr[middle] > el) {
+            end = middle - 1;
+        } else {
+            return middle;
+        }
+    }
+
+    return -1;
+};
+
+console.log(binarySearch([1, 3, 4, 5, 7, 10, 14, 17, 20], 5));
